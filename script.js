1dcf5087f892bdae880ced07c04a5b6229320bbb
Abilash <90945323+AbilashMugunth@users.noreply.github.com>
2023-12-07 13:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let para = document.querySelector('#status')
+document.querySelector('#enterBtn').addEventListener("click",()=>{
+	para.innerText='';
+	let h1 = document.createElement('h1');
+	h1.innerText ='Entered Metaverse';
+	para.appendChild(h1);
+	
+})
